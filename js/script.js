ae70e519292d0280b95c86566a57a0cdc452b7f1
AugustoM94 <email@github.com>
2023-10-16 17:52:46 +0200

    Project completed

--- js/script.js
+++ js/script.js
@@ -5,11 +5,20 @@ let stop = false;
 while (!stop) {
 
     input = prompt("Inserisci un prodotto da acquistare o digita STOP per terminare") 
-
     if (input.toUpperCase() === "STOP") {
     stop = true;
+    }  else if (input !== '') {
+        listaEl.push(input);
     }
-    listaEl.push(input);
+
 }
 console.log(listaEl);
 
+
+let li = document.getElementById('list')
+
+for (let i = 0; i < listaEl.length; i++) {
+    let listItem = document.createElement('li');
+    listItem.innerHTML = listaEl[i];
+    li.appendChild(listItem);
+}
