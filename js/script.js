6e00d403239463177e6f2eb61c4b27943c3f9de0
AugustoM94 <email@github.com>
2023-10-16 16:52:59 +0200

    created ciclo while

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 let listaEl = [];
+let input;
+let stop = false;
+
+while (!stop) {
+
+    input = prompt("Inserisci un prodotto da acquistare o digita STOP per terminare") 
+
+    if (input.toUpperCase() === "STOP") {
+    stop = true;
+    }
+    listaEl.push(input);
+}
+console.log(listaEl);
+
